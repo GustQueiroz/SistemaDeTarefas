6ce17c26d79d309216a7fddc212f092917da74f7
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-03-10 18:02:07 -0300

    map

--- obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7ce046da9333ff9040dafa3e63840c15e1b8e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af54261d9660950fecf0c9b9e4d4a4f57180778")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
