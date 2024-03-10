cfa6a7cdb56966af35ea665c79bd00042dbcf170
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-03-10 16:50:28 -0300

    dbcontext

--- Repositorios/Interfaces/IUsuarioRepositorio.cs
+++ Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -1,6 +1,13 @@
-﻿namespace SistemaDeTarefas.Repositorios.Interfaces
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
 {
     public interface IUsuarioRepositorio
     {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(int id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
     }
 }
---
+++ Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContex _dbContext;
+        public UsuarioRepositorio(SistemaTarefasDBContex sistemaTarefasDBContex)
+        {
+            _dbContext = sistemaTarefasDBContex;
+        }
+
+        public Task<UsuarioModel> BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
